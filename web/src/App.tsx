dbbc3d75c4b9ff091d6421432de2d89ca6c60ef6
dexteresc <jonathan.jeding@gmail.com>
2022-11-11 14:17:06 +0000

    add chakra

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,37 +1,104 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
+import { useEffect, useState } from 'react'
+import {
+  Box,
+  Text,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+  Container,
+  Button,
+  Select,
+  Input,
+  ButtonGroup,
+} from '@chakra-ui/react'
 function App() {
-  const [count, setCount] = useState(0)
+  interface LadokObject {
+    id: number
+    socialSecurity: string
+    courseCode: string;
+    courseModule: string;
+    grade: string;
+    date: string;
+    status: "draft" | "done" | "certified" | "obstacle"
+  }
 
-  // fetch http://127.0.0.1:3000/
-  fetch('http://127.0.0.1:3000/').then(res => res.json()).then(console.log)
+  const [ladok, setLadok] = useState<LadokObject[]>([])
 
+  useEffect(() => {
+    fetch('http://localhost:3001/results').then(res => res.json()).then(setLadok)
+    console.log(ladok)
+  }, [])
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Box padding="2">
+      <Box display="flex" flexDirection="row" justifyContent="space-between">
+        <Box>
+          <Text marginBottom="2" fontWeight="bold">Markera</Text>
+          <ButtonGroup variant="outline" spacing="2">
+            <Button>Alla</Button>
+            <Button>Betygsatta</Button>
+            <Button>Inga</Button>
+          </ButtonGroup>
+        </Box>
+        <Box>
+
+          <Text marginBottom="2" fontWeight="bold">Datum för markerade</Text>
+          <Box display="flex" flexDirection="row">
+            <Input
+              placeholder="Datum"
+              size="md"
+              type="date"
+            />
+            <Button>Sätt datum</Button>
+          </Box>
+        </Box>
+        <Box display="flex" flexDirection="row">
+          <Text>Spara som</Text>
+          <Select placeholder='Select option'>
+            {
+              ["draft", "done", "certified", "obstacle"].map((status) => (
+                <option key={status} value={status}>{status}</option>
+              ))
+            }
+          </Select>
+          <Button>Överför</Button>
+        </Box>
+      </Box>
+      <TableContainer>
+        <Table variant='simple'>
+          <Thead>
+            <Tr>
+              <Th>Namn</Th>
+              <Th>Omdöme</Th>
+              <Th>Betyg i Canvas</Th>
+              <Th>Betyg i Ladok</Th>
+              <Th>Examinationsdatum</Th>
+              <Th>Status</Th>
+              <Th>Information</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {ladok.map((item, index) => (
+              <Tr key={index}>
+                <Td>{item.socialSecurity}</Td>
+                <Td>{item.courseCode}</Td>
+                <Td>{item.courseModule}</Td>
+                <Td>{item.grade}</Td>
+                <Td>{item.date}</Td>
+                <Td>{item.status}</Td>
+                <Td></Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </Box>
   )
 }
 
