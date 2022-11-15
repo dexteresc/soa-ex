e90120ecfb3399d6ad61ccc5af7b9de756d9d2d5
dexteresc <jonathan.jeding@gmail.com>
2022-11-15 23:55:45 +0000

    add snackbar context

---
+++ web/src/contexts/snackbar.tsx
@@ -0,0 +1,34 @@
+import { Alert, AlertProps, Snackbar } from "@mui/material";
+import { createContext, useState, useContext } from "react";
+
+interface SnackbarContext {
+  setSnackbar: (snackbar: Pick<AlertProps, "children" | "severity"> | null) => void;
+}
+
+const SnackbarContext = createContext<SnackbarContext | null>(null);
+
+export const SnackbarProvider = ({ children }: { children: React.ReactNode }) => {
+  const [snackbar, setSnackbar] = useState<Pick<AlertProps, "children" | "severity"> | null>(null);
+  const handleCloseSnackbar = () => {
+    setSnackbar(null);
+  };
+
+  return (
+    <SnackbarContext.Provider value={{ setSnackbar }}>
+      {children}
+      {!!snackbar && (
+        <Snackbar
+          open
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+          onClose={handleCloseSnackbar}
+          autoHideDuration={6000}
+        >
+          <Alert {...snackbar} onClose={handleCloseSnackbar} />
+        </Snackbar>
+      )}
+    </SnackbarContext.Provider>
+  );
+};
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+export const useSnackbar = () => useContext(SnackbarContext)!;
