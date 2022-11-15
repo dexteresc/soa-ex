7b89798079a5932498f703d98052b7029a6e8e15
dexteresc <jonathan.jeding@gmail.com>
2022-11-15 20:33:13 +0000

    mv theme

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -6,7 +6,6 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "./index.css";
-import { createTheme, CssBaseline, ThemeProvider, useMediaQuery } from "@mui/material";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
