57d66fab5a639606437bde769cbeb6944c3e436a
dexteresc <jonathan.jeding@gmail.com>
2022-11-17 15:12:30 +0000

    fix modal triggers action-open

--- web/src/components/Results.tsx
+++ web/src/components/Results.tsx
@@ -7,27 +7,28 @@ import {
   Modal,
   Autocomplete,
   TextField,
-  IconButton,
   Button,
   Paper,
   SpeedDialIcon,
   SpeedDial,
   SpeedDialAction,
   Backdrop,
+  ModalProps,
+  Fade,
 } from "@mui/material";
 import { GridColDef, GridCellParams, DataGrid, useGridApiContext } from "@mui/x-data-grid";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useStudyResults, StudyResult } from "../contexts/study-results";
 import SchoolIcon from "@mui/icons-material/School";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
-import CloseIcon from "@mui/icons-material/Close";
 import TaskIcon from "@mui/icons-material/Task";
+import DangerousOutlinedIcon from "@mui/icons-material/DangerousOutlined";
 
 export default function Results() {
   const { setChangedStudyResults, changedStudyResults, loading, error, studyResults } = useStudyResults();
   const [modalOpen, setModalOpen] = useState(false);
-  const [modalContent, setModalContent] = useState<JSX.Element | null>();
+  const [modalContent, setModalContent] = useState<JSX.Element>(<></>);
   const [actionOpen, setActionOpen] = useState(false);
   const actions = [
     { icon: <TaskIcon />, name: "Lägg till resultat", onClick: () => openAddResult() },
@@ -35,40 +36,9 @@ export default function Results() {
     { icon: <SchoolIcon />, name: "Lägg till kurs", onClick: () => openAddCourse() },
   ];
 
-  const handleActionClose = () => {
-    setActionOpen(false);
-  };
-
   const openAddResult = () => {
-    console.log("Add result");
-    setModalOpen(true);
     setModalContent(
-      <Paper
-        sx={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          width: "80vw",
-          p: 4,
-          display: "flex",
-          flexDirection: "column",
-          gap: 2,
-        }}
-      >
-        <Box>
-          <Typography variant="h4">Lägg till nytt resultat</Typography>
-          {/* close button */}
-          <IconButton
-            size="small"
-            color="secondary"
-            aria-label="close"
-            onClick={() => setModalOpen(false)}
-            sx={{ position: "absolute", top: 10, right: 10 }}
-          >
-            <CloseIcon />
-          </IconButton>
-        </Box>
+      <Box component="form">
         {/* Student name */}
         <Autocomplete
           options={studyResults.map((result) => result.name)}
@@ -110,7 +80,7 @@ export default function Results() {
             Spara
           </Button>
         </Box>
-      </Paper>
+      </Box>
     );
   };
 
@@ -120,6 +90,24 @@ export default function Results() {
 
   const openAddCourse = () => {
     // TODO
+  };
+
+  const handleActionClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>, onClick?: () => void) => {
+    event.preventDefault();
+    if (onClick) onClick();
+    setModalOpen(true);
+    setActionOpen((prev) => !prev);
+    if (document.activeElement?.tagName === "BUTTON") {
+      (document.activeElement as HTMLButtonElement).blur();
+    }
+  };
+
+  const handleActionClose = () => {
+    setActionOpen(false);
+  };
+
+  const handleActionOpen = () => {
+    setActionOpen(true);
   };
 
   const columns: GridColDef[] = [
@@ -172,71 +160,81 @@ export default function Results() {
     [changedStudyResults]
   );
 
+  useEffect(() => {
+    console.log(actionOpen);
+  }, [actionOpen]);
+
   return (
-    <Box
-      className="transition"
-      sx={
-        loading
-          ? {
-              height: "100vh",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }
-          : { height: "calc(100vh - 64px)" }
-      }
-    >
-      {error ? (
-        <Typography variant="h4" color="error">
-          {error.message}
-        </Typography>
-      ) : loading ? (
-        <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" viewBox="0 0 38 38" stroke="#000">
-          <g fill="none" fillRule="evenodd">
-            <g transform="translate(1 1)" strokeWidth="2">
-              <circle strokeOpacity=".5" cx="18" cy="18" r="18" />
-              <path d="M36 18c0-9.94-8.06-18-18-18">
-                <animateTransform
-                  attributeName="transform"
-                  type="rotate"
-                  from="0 18 18"
-                  to="360 18 18"
-                  dur="1s"
-                  repeatCount="indefinite"
-                />
-              </path>
+    <Box>
+      <Box
+        className="transition"
+        sx={
+          loading || error
+            ? {
+                height: "100vh",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }
+            : { height: "calc(100vh - 64px)" }
+        }
+      >
+        {error ? (
+          <Box
+            paddingBottom={10}
+            color="error.main"
+            sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+          >
+            <DangerousOutlinedIcon sx={{ fontSize: 100, paddingBottom: 5 }} />
+            <Typography variant="h4">{error.message}</Typography>
+          </Box>
+        ) : loading ? (
+          <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" viewBox="0 0 38 38" stroke="#000">
+            <g fill="none" fillRule="evenodd">
+              <g transform="translate(1 1)" strokeWidth="2">
+                <circle strokeOpacity=".5" cx="18" cy="18" r="18" />
+                <path d="M36 18c0-9.94-8.06-18-18-18">
+                  <animateTransform
+                    attributeName="transform"
+                    type="rotate"
+                    from="0 18 18"
+                    to="360 18 18"
+                    dur="1s"
+                    repeatCount="indefinite"
+                  />
+                </path>
+              </g>
             </g>
-          </g>
-        </svg>
-      ) : (
-        <DataGrid
-          rows={changedStudyResults}
-          columns={columns}
-          checkboxSelection
-          experimentalFeatures={{
-            newEditingApi: true,
-          }}
-          disableColumnMenu
-          disableColumnFilter
-          processRowUpdate={processRowUpdate}
-          onProcessRowUpdateError={console.log}
-        />
-      )}
-      {modalOpen && (
-        <Modal open={modalOpen} onClose={() => setModalOpen(false)}>
-          {modalContent ? modalContent : <Typography variant="h4">Modal content</Typography>}
-        </Modal>
-      )}
+          </svg>
+        ) : (
+          <DataGrid
+            rows={changedStudyResults}
+            columns={columns}
+            checkboxSelection
+            experimentalFeatures={{
+              newEditingApi: true,
+            }}
+            disableColumnMenu
+            disableColumnFilter
+            processRowUpdate={processRowUpdate}
+            onProcessRowUpdateError={console.log}
+          />
+        )}
+      </Box>
+      <ActionModal title="Testing" open={modalOpen} onClose={() => setModalOpen(false)} content={modalContent} />
+
       <Box sx={{ height: 330, flexGrow: 1 }}>
         <Backdrop open={actionOpen} />
         <SpeedDial
-          ariaLabel="SpeedDial tooltip example"
+          ariaLabel="actions menu"
           sx={{ position: "absolute", bottom: 69, right: 16 }}
           icon={<SpeedDialIcon />}
-          onClose={() => setActionOpen(false)}
-          onOpen={() => {
-            setActionOpen(true);
-          }}
+          onBlur={handleActionClose}
+          onClick={(e) => handleActionClick(e)}
+          onClose={handleActionClose}
+          onFocus={handleActionOpen}
+          onMouseEnter={handleActionOpen}
+          onMouseLeave={handleActionClose}
           open={actionOpen}
         >
           {actions.map((action) => (
@@ -245,7 +243,9 @@ export default function Results() {
               icon={action.icon}
               tooltipTitle={action.name}
               tooltipOpen
-              onClick={() => action.onClick()}
+              onClick={(e) => {
+                handleActionClick(e, action.onClick);
+              }}
             />
           ))}
         </SpeedDial>
@@ -267,5 +267,42 @@ const SelectEditCell = ({ id, field, value }: GridCellParams) => {
       <MenuItem value="certified">certified</MenuItem>
       <MenuItem value="obstacle">obstacle</MenuItem>
     </Select>
+  );
+};
+
+const ActionModal = ({
+  title,
+  open,
+  content,
+  onClose,
+  ...props
+}: {
+  title: string;
+  content: React.ReactNode;
+  children?: React.ReactNode;
+} & Omit<ModalProps, "children">) => {
+  return (
+    <Modal open={open} onClose={onClose} {...props} closeAfterTransition>
+      <Fade in={open}>
+        <Paper
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: "80vw",
+            p: 4,
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+          }}
+        >
+          <Box>
+            <Typography variant="h4">{title}</Typography>
+          </Box>
+          {content}
+        </Paper>
+      </Fade>
+    </Modal>
   );
 };
