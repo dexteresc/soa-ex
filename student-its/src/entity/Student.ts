af35cd032173dbb51184ba1384faf58e8f2b87ae
dexteresc <jonathan.jeding@gmail.com>
2022-11-24 14:17:16 +0000

    fix format

--- student-its/src/entity/Student.ts
+++ student-its/src/entity/Student.ts
@@ -1,17 +1,16 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
 @Entity()
 export class Student {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number
+  @Column({ unique: true })
+  studentId: string;
 
-    @Column({ unique: true })
-    studentId: string
+  @Column({ unique: true })
+  socialSecurity: string;
 
-    @Column({ unique: true })
-    socialSecurity: string
-
-    @Column()
-    name: string
+  @Column()
+  name: string;
 }
